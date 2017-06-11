32dffa708e574793bc58bc9221135a7a5ea8bc01
lijiaming <lijiaming1984@gmail.com>
2017-06-11 22:31:21 +0800

    add init

--- src/main/java/com/example/springmvc/handlerinterceptor/Application.java
+++ src/main/java/com/example/springmvc/handlerinterceptor/Application.java
@@ -2,9 +2,14 @@ package com.example.springmvc.handlerinterceptor;
 
 import com.example.springmvc.handlerinterceptor.handlerinterceptor.LogCookieHandlerInterceptor;
 import com.example.springmvc.handlerinterceptor.handlerinterceptor.LogTimeHandlerInterceptor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -16,6 +21,20 @@ public class Application {
     }
 }
 
+@Component
+@Slf4j
+class InitRunner implements CommandLineRunner {
+    private final String initMsg;
+    @Autowired
+    public InitRunner(@Value("${init.message}") String initMsg) {
+        this.initMsg = initMsg;
+    }
+    @Override
+    public void run(String... strings) throws Exception {
+        log.info(">>>>>>>>>>>> init message is {}",this.initMsg);
+    }
+}
+
 @Configuration
 class WebConfig extends WebMvcConfigurerAdapter {
     @Override
@@ -24,4 +43,5 @@ class WebConfig extends WebMvcConfigurerAdapter {
         registry.addInterceptor(new LogCookieHandlerInterceptor());
         super.addInterceptors(registry);
     }
+
 }
